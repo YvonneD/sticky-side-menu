c0b7d06da8028db81872519ed0b3918e940adff6
YvonneD <diyifang@ymail.com>
2019-03-27 00:37:33 -0400

    v1

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # sticky-side-menu
 several versions of color themes with Javascript
+
+
+![Alt Text](https://1.bp.blogspot.com/-AOJ-GvPnMkw/XJr59Y8it3I/AAAAAAAACvc/uQPfd5QagVUt05dPrwOL7UC0yp6709kLACLcBGAs/s320/1.gif)
+
+blog https://yifangdi.blogspot.com/2019/03/sticky-side-menu-with-javascript.html
---
+++ js/jquery.ss.menu.js
@@ -0,0 +1,80 @@
+/*!  Plugin: ssMenu (Sticky Side Navigation)
+ *   Author: Asif Mughal
+ *   URL: www.codehim.com
+ *   License: MIT License
+ *   Copyright (c) 2019 - Codehim
+ */
+/* File: jquery.ss.emenu.js */
+(function ($) {
+	$.fn.ssMenu = function (options) {
+
+		var setting = $.extend({
+			theme: "default", //put the name of theme in string       
+			additionalCSS: ({
+				'background': '', //custom background color
+				'color': '', //custom text color 
+				'boxShadow': '', //to add box shadow 
+				'textShadow': '', //to add text shadow 
+			}),
+
+		}, options);
+
+		return this.each(function () {
+
+			var target = $(this);
+
+			var ssMenu = $(".ss-menu");
+
+			var Triggers = $(ssMenu).find("li");
+
+
+			//Applying customizations 
+
+			$(target).addClass(setting.theme);
+
+			$(target).css(setting.additionalCSS);
+
+
+			$(Triggers).mouseenter(function () {
+
+
+				//to avoid the adding classes again and again 
+				if ($(ssMenu).hasClass("open")) {
+
+					return;
+				}
+
+				$(ssMenu).addClass("open");
+
+
+			});
+
+
+			$(Window).click(function (e) {
+
+				if ($(e.target).closest(Triggers).length) {
+
+					return;
+
+				}
+
+
+				$(ssMenu).removeClass("open");
+
+
+			});
+
+			
+
+
+		});
+	};
+
+})(jQuery);
+/*   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
